a7edebaa380da53fc13a85c7bb4c4a7153210794
FantzFoXx <FantzFoXx@FantzFoXxs-Mac.local>
2016-02-06 16:13:52 +0100

    save push

--- src/print_ls.c
+++ src/print_ls.c
@@ -20,24 +20,34 @@ static void		print_basic_item(char *s)
 	ft_putendl(s);
 }
 
+static char		*get_rights(mode_t mode)
+{
+	char *rights;
+
+	rights = ft_strdup((S_ISDIR(mode) ? "d" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IRUSR) ? "r" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IWUSR) ? "w" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IXUSR) ? "x" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IRGRP) ? "r" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IWGRP) ? "w" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IXGRP) ? "x" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IROTH) ? "r" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IWOTH) ? "w" : "-"));
+	rights = ft_strjoin(rights, ((mode & S_IXOTH) ? "x" : "-"));
+	return (rights);
+
+}
+
 static void		print_list_item(t_dir_item *item)
 {
+	char	*rights;
 	char	**date;
 	DIR		*cur_dir;
 	int		i;
 	i = 0;
-	date = NULL;
-	ft_putchar((S_ISDIR(item->prop.st_mode) ? 'd' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IRUSR) ? 'r' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IWUSR) ? 'w' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IXUSR) ? 'x' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IRGRP) ? 'r' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IWGRP) ? 'w' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IXGRP) ? 'x' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IROTH) ? 'r' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IWOTH) ? 'w' : '-'));
-	ft_putchar(((item->prop.st_mode & S_IXOTH) ? 'x' : '-'));
-	ft_putchar(' ');
+	rights = get_rights(item->prop.st_mode);
+	ft_putstr(rights);
+	ft_putstr("  ");
 	cur_dir = opendir(ft_strjoin(item->path, item->item_name));
 	if (cur_dir)
 		while (readdir(cur_dir))
@@ -45,18 +55,18 @@ static void		print_list_item(t_dir_item *item)
 	else
 		i = 1;
 	ft_putnbr(i);
-	ft_putchar(' ');
+	ft_putstr("  ");
 	ft_putstr(getpwuid(item->prop.st_uid)->pw_name);
-	ft_putchar(' ');
+	ft_putstr("  ");
 	ft_putstr(getgrgid(item->prop.st_gid)->gr_name);
-	ft_putchar(' ');
+	ft_putstr("  ");
 	ft_putnbr(item->prop.st_size);
-	ft_putchar(' ');
+	ft_putstr("  ");
 	date = ft_strsplit(ctime(&item->prop.st_mtimespec.tv_sec), ' ');
 	ft_putstr(date[1]);
-	ft_putchar(' ');
+	ft_putstr("  ");
 	ft_putstr(date[2]);
-	ft_putchar(' ');
+	ft_putstr("  ");
 	i = -1;
 	while (++i < 5)
 		ft_putchar(date[3][i]);
@@ -67,7 +77,7 @@ static void		print_list_item(t_dir_item *item)
 		closedir(cur_dir);
 }
 
-static void		print_all_items(t_dir_item *items, char *params)
+void			print_ls(t_dir_item *items, char *params)
 {
 	int i;
 
@@ -76,24 +86,8 @@ static void		print_all_items(t_dir_item *items, char *params)
 		i = 1;
 	while (items)
 	{
-		if (i)
-			print_list_item(items);
-		else
-			print_basic_item(items->item_name);
-		items = items->next;
-	}
-}
-
-static void		print_items(t_dir_item *items, char *params)
-{
-	int i;
-
-	i = 0;
-	if (ft_strchr(params, 'l'))
-		i = 1;
-	while (items)
-	{
-		if (items->item_name[0] != '.')
+		if ((items->item_name[0] == '.' && ft_strchr(params, 'a'))
+				|| items->item_name[0] != '.')
 		{
 			if (i)
 				print_list_item(items);
@@ -103,13 +97,3 @@ static void		print_items(t_dir_item *items, char *params)
 		items = items->next;
 	}
 }
-
-void			print_ls(t_dir_item *items, char *params)
-{
-	if (ft_strchr(params, 'a'))
-		print_all_items(items, params);
-	else
-		print_items(items, params);
-}
-
-
