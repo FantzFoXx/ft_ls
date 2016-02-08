a0fe86371cdc3f9765038eacab341dce213aa34f
Ugo DELORME <udelorme@e3r11p4.42.fr>
2016-02-08 17:09:48 +0100

    nouveau fonctionnement des donnees pour option l

--- includes/option_l.h
+++ includes/option_l.h
@@ -1,5 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   option_l.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: udelorme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/08 12:49:31 by udelorme          #+#    #+#             */
+/*   Updated: 2016/02/08 17:08:45 by udelorme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef OPTION_L_H
 # define OPTION_L_H
+
+typedef struct	s_foo
+{
+	char			*str;
+	struct s_foo	*next;
+}				t_foo;
+
 
 void	print_ls_l(t_dir_item *items, char *params);
 
--- src/option_l.c
+++ src/option_l.c
@@ -1,9 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   option_l.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: udelorme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/08 12:49:04 by udelorme          #+#    #+#             */
+/*   Updated: 2016/02/08 17:08:44 by udelorme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 #include <time.h>
 #include <pwd.h>
 #include <grp.h>
+#include "option_l.h"
 
-static char		*get_rights(mode_t mode)
+static t_foo	*t_foo_new(char *str)
+{
+	t_foo *new;
+
+	new = (t_foo *)malloc(sizeof(t_foo));
+	if (new)
+	{
+			new->str = ft_strdup(str);
+			new->next = NULL;
+	}
+	return (new);
+}
+
+static void		t_foo_push(t_foo **first, t_foo *new)
+{
+	t_foo *index;
+
+	index = *first;
+	if (!index)
+		*first = new;
+	else
+	{
+		while (index->next)
+			index = index->next;
+		index->next = new;
+	}
+}
+
+static t_foo	*get_rights(mode_t mode)
 {
 	char *rights;
 
@@ -17,7 +58,7 @@ static char		*get_rights(mode_t mode)
 	rights = ft_strjoin(rights, ((mode & S_IROTH) ? "r" : "-"));
 	rights = ft_strjoin(rights, ((mode & S_IWOTH) ? "w" : "-"));
 	rights = ft_strjoin(rights, ((mode & S_IXOTH) ? "x" : "-"));
-	return (rights);
+	return (t_foo_new(rights));
 }
 
 static	int		count_elems(t_dir_item *item)
@@ -37,42 +78,53 @@ static	int		count_elems(t_dir_item *item)
 	return (i);
 }
 
-static void		print_list_item(t_dir_item *item)
-{
-	char	*rights;
-	char	**date;
-	int		i;
-	i = 0;
-	rights = get_rights(item->prop.st_mode);
+/*
 	ft_putstr(rights);
-	ft_putstr("  ");
 	i = count_elems(item);
 	ft_putnbr(i);
-	ft_putstr("  ");
 	ft_putstr(getpwuid(item->prop.st_uid)->pw_name);
-	ft_putstr("  ");
 	ft_putstr(getgrgid(item->prop.st_gid)->gr_name);
-	ft_putstr("  ");
 	ft_putnbr(item->prop.st_size);
-	ft_putstr("  ");
-	date = ft_strsplit(ctime(&item->prop.st_mtimespec.tv_sec), ' ');
 	ft_putstr(date[1]);
-	ft_putstr("  ");
 	ft_putstr(date[2]);
-	ft_putstr("  ");
+*/
+
+static t_foo		*get_date(t_dir_item *item)
+{
+	char	**tmp;
+	t_foo	*date;
+
+	date = NULL;
+	tmp = ft_strsplit(ctime(&item->prop.st_mtimespec.tv_sec), ' ');
+	t_foo_push(&date, t_foo_new(tmp[1]));
+	t_foo_push(&date, t_foo_new(tmp[2]));
+	t_foo_push(&date, t_foo_new(ft_strsub(tmp[3], 0, 5)));
+	ft_freetab(tmp);
+	return (date);
+}
+
+static t_foo		*print_list_item(t_dir_item *item)
+{
+	t_foo *line;
+	int		i;
+	i = 0;
+	line = NULL;
+	t_foo_push(&line, get_rights(item->prop.st_mode));
+	t_foo_push(&line, t_foo_new(ft_itoa(count_elems(item))));
+	t_foo_push(&line, t_foo_new(getpwuid(item->prop.st_uid)->pw_name));
+	t_foo_push(&line, t_foo_new(getgrgid(item->prop.st_gid)->gr_name));
+	t_foo_push(&line, get_date(item));
+	t_foo_push(&line, t_foo_new(item->item_name));
 	i = -1;
-	while (++i < 5)
-		ft_putchar(date[3][i]);
-	ft_putchar(' ');
-	ft_putstr(item->item_name);
-	ft_putendl("");
+	return (line);
 }
 
 void			print_ls_l(t_dir_item *items, char *params)
 {
 	int	a;
-	char ***line;
-	line = NULL;
+	t_list *container;
+	t_foo *index;
+	container = NULL;
 	a = 0;
 	if (ft_strchr(params, 'a'))
 		a = 1;
@@ -80,7 +132,23 @@ void			print_ls_l(t_dir_item *items, char *params)
 	{
 		if ((items->item_name[0] == '.' && a)
 				|| items->item_name[0] != '.')
-			print_list_item(items);
+			ft_lstpush(&container, ft_lstnew(print_list_item(items), sizeof(t_foo)));
 		items = items->next;
+	}
+	index = NULL;
+	ft_putstr("total ");
+
+	while (container)
+	{
+		index = container->content;
+		while (index)
+		{
+			ft_putstr((char *)index->str);
+			if (index->next)
+				ft_putchar(' ');
+			index = index->next;
+		}
+		container = container->next;
+		ft_putchar('\n');
 	}
 }
